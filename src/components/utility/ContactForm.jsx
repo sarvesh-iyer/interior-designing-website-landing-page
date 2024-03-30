5e2b80e8077e06963e6f3323bee9d19512566805
sarvesh-iyer <sarvesh.iyer3@gmail.com>
2024-03-30 19:51:13 +0530

    email.js package added, and form submit implemented using emailjs

--- src/components/utility/ContactForm.jsx
+++ src/components/utility/ContactForm.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import { SuccessModal } from "./SuccessModal"
+import emailjs from '@emailjs/browser';
 
 export const ContactForm = () => {
 
@@ -18,20 +19,28 @@ export const ContactForm = () => {
         phone: false,
         query: false
     })
+    const [isSubmitting, setIsSubmitting] = useState(false)
+
     const nameRef = useRef(null)
     const emailRef = useRef(null)
     const phoneRef = useRef(null)
     const queryRef = useRef(null)
-
-    const errorFieldStyle = "1px solid var(--primary-color)"
-    const defaultBorderStyle = "1px solid #ccc"
+    const formRef = useRef(null)
 
     const handleInputChange = (e) => {
         let {name, value} = e.target
+        // for phone no. field only numbers are allowed
+        const regex = /^[0-9\s]*$/;
+        if(name === 'phone' && !(regex.test(value?.trim()))) return  
+
         setInputData({
             ...inputData,
             [name]: value
         })
+        if(name === 'name' && value?.trim() !== "") nameRef.current.classList.remove('error-field')
+        if(name === 'email' && value?.trim() !== "") emailRef.current.classList.remove('error-field')
+        if(name === 'phone' && value?.trim() !== "") phoneRef.current.classList.remove('error-field')
+        if(name === 'query' && value?.trim() !== "") queryRef.current.classList.remove('error-field')
     } 
 
     const isFormValid = () => {
@@ -39,27 +48,38 @@ export const ContactForm = () => {
             inputData.name?.trim() === "" || inputData.email?.trim() === "" || 
             inputData.phone?.trim() === "" || inputData.query?.trim() === ""
         ){
-            if(inputData.name?.trim() === "")  nameRef.current.style.borderBottom = errorFieldStyle
-            if(inputData.email?.trim() === "") emailRef.current.style.borderBottom = errorFieldStyle
-            if(inputData.phone?.trim() === "") phoneRef.current.style.borderBottom = errorFieldStyle
-            if(inputData.query?.trim() === "") queryRef.current.style.borderBottom = errorFieldStyle
+            if(inputData.name?.trim() === "")  nameRef.current.classList.add('error-field')
+            if(inputData.email?.trim() === "") emailRef.current.classList.add('error-field')
+            if(inputData.phone?.trim() === "") phoneRef.current.classList.add('error-field')
+            if(inputData.query?.trim() === "") queryRef.current.classList.add('error-field')
             return false
         }
 
         return true
     } 
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async(e) => {
         e.preventDefault()
         if(!isFormValid()) return
+        try{
+            setIsSubmitting(true)
+            let resposne = await emailjs.sendForm('service_62zihjm', 'template_gkt0hkj', formRef.current, {
+                publicKey: 'VQ1BJ5tm7Oszi_2Y4',
+            })
 
-        setInputData(initalInputState)
-        setShowSuccessModal(true)
+            setIsSubmitting(false)
+            setInputData(initalInputState)
+            setShowSuccessModal(true)
+
+        }catch(e){
+            console.log(e)
+        }
+
     } 
 
     return (
         <>
-            <form className="form-wrapper">
+            <form ref={formRef} className="form-wrapper" onSubmit={handleSubmit}>
                 <h2 className="title">Contact us</h2>
                 <div className="form-item-row">
                     <input 
@@ -71,6 +91,7 @@ export const ContactForm = () => {
                         id="name"
                         placeholder="Enter your name"
                         onChange={handleInputChange}
+                        maxLength={200}
                     />
                 </div>
                 <div className="form-item-row">
@@ -79,10 +100,11 @@ export const ContactForm = () => {
                         value={inputData.email}
                         ref={emailRef}
                         type="email" 
-                        name="email" 
+                        name="email"
                         id="email"
                         placeholder="Enter your email"
                         onChange={handleInputChange}
+                        maxLength={200}
                     />
                 </div>
                 <div className="form-item-row">
@@ -90,8 +112,10 @@ export const ContactForm = () => {
                         className="form-inputs"
                         value={inputData.phone}
                         ref={phoneRef}
-                        type="number" 
+                        type="tel" 
+                        maxLength={10}
                         name="phone" 
+                        pattern="\d{10}"
                         id="phone"
                         placeholder="Enter your mobile number"
                         onChange={handleInputChange}
@@ -108,10 +132,15 @@ export const ContactForm = () => {
                         onChange={handleInputChange}
                         cols="30" 
                         rows="8"
+                        maxLength={500}
                     ></textarea>
                 </div>
                 <div className="form-item-row">
-                    <button className="submit-btn" onClick={handleSubmit}>Submit</button>
+                    <input 
+                        className="submit-btn" 
+                        type="submit" 
+                        value={isSubmitting ? "Submitting..." : "Submit"} 
+                    />
                 </div>
             </form>
 
